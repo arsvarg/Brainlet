9de38373bce6d34471aa1ba1cf991a8378708ae7
arsvarg <arsvarg8@gmail.com>
2021-02-16 23:13:36 +0700

    Initial
    Слыш, работать

---
+++ Brainlet/Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletForce;
+    [SerializeField] float fireRate;
+    [SerializeField] float damage;
+
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        FindObjectOfType<Player_shooting>().fireRate = fireRate;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Box>())
+        {
+            collision.gameObject.GetComponent<Box>().TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, FindObjectOfType<Player_movement>().transform.position) > 50f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
