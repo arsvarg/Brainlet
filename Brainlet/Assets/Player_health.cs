803ea204b832ae69823687f2e0451fd60574d8cf
arsvarg <arsvarg8@gmail.com>
2021-02-17 01:35:59 +0700

    Jaws
    Добавил шипы. Наносят урон раз в полсекунды, когда на них стоишь.

---
+++ Brainlet/Assets/Player_health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_health : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    float currentHealth;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth<=0)
+        {
+            Die();
+        }
+        
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
